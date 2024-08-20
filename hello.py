bb10f38bebc1c06672619e5534cc10b552a36773
imfurqan012 <imfurqan012@gmail.com>
2024-08-20 12:32:17 +0500

    update

--- hello.py
+++ hello.py
@@ -1,20 +1,116 @@
-Education=int(input("Enter Your Education: "))
-Age=int(input("Enter Your Age: "))
-Height=float(input("Enter Your Height: "))
+# Education=int(input("Enter Your Education: "))
+# Age=int(input("Enter Your Age: "))
+# Height=float(input("Enter Your Height: "))
 
 
-#if(Education>=14 and Age>=18 and Heights>=5)
-   # print("Eligible")
-#else:
-   # print("Not Eligible") 
+# if (Education>=14 and Age>=18):
+#     if(Height>=5):
+#         print("You are Elligible")
+#     else:
+#         print("You are not Elligible")    
+# else:
+#     print("You are Elligible")
+                  
+
+# if((Education>=16 and Age>=18 )or(Education>=16 and Height>=5)or(Age>=18 and Height>=5)):
+#     print("You are Eligible")
+# else:
+#     print("You are not Eligible")
+
+# tup=("Hello","world")
+# print(type(tup))
+# print(tup)
+
+# tup1=("Hello world",)
+# print(type(tup1))
+# print(tup1)
+
+# tup=(1,2,3,4)
+# print(len(tup))
+# print(tup[0])
+
+# y=list(tup)
+# print(type(y))
+# print(y)
+
+# y.append(5)
+# print(y)
+# y.insert(1,5)
+# print(y)
+# y.pop(2)
+# print(y)
+# tup=tuple(y)
+# print(tup)
+# y=list(tup)
+# print(y)
+   
+# thisdict={"name":"John Doe","Age":26,"city":"New York"}
+# print(thisdict["Age"])
+# print(thisdict)
+# print(thisdict["name"])
+# print(thisdict["city"])
+# thisdict["name"]="Ashutosh"
+# print(thisdict["name"])
+# thisdict["first name"]="Furqan"
+# print(thisdict)
+# x=thisdict.get("name")
+# print(x)
 
 
-if((Education>=16 and Age>=18 )or(Education>=16 and Height>=5)or(Age>=18 and Height>=5)):
-    print("You are Eligible")
-else:
-    print("You are not Eligible")
-    
+# set1={1,2,3,4,5,6}
+# set2={5,6,7,8,9,10}
+# set3={10,11,12,13,14,15,16,17}
+# set1.add(5)
+# print(set1)
+# set1.update(set2)
+# print(set1)
+# print(type(set1))
+# set3=set({1,2,3,4,5})
+# print(set3)
+# set1={1,2,3,4,5}
+# set2={"hello", "world"}
+# set1.update(set2)
+# print(set1)
+# set1.add(6)
+# print(set1)
+# set1.remove("hello")
+# print(set1)
+# set1.discard(9)
+# print(set1)
+
+# set4=set1.union(set2,set3)
+# print(set4)
+# set4=set1|set2|set3
+# print(set4)
+
+# set4=set1.intersection(set2,set3)
+# print(set4)
+# set4=set1&set2
+# print(set4)
+
+# set4=set1|set2&set3
+# print(set4)
+
+# set4=set1.difference(set2)
+# print(set4)
+# set4=set1-set2
+# print(set4)
+
+# set1={1,2,3,4}
+# set2={5,6,7,8,9}
+# print(1 in set1)
+# print(1 not in set1)
+# print(set1.union(set2,set3))
 
 
-       
+
+
+
+
+
+
+
+
+
+
 
